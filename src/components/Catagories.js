51d2dc320a319e1b2c1b00794676220449f9e10d
Zelalem1222 <zelalemfissha394@gmail.com>
2022-07-20 13:50:00 -0400

    Modify catagories.js

--- src/components/Catagories.js
+++ src/components/Catagories.js
@@ -1,9 +1,23 @@
+/* eslint-disable */
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/catagories/catagories';
 
-function Catagories() {
+const Catagories = () => {
+  const status = useSelector((state) => state.catagories);
+   console.log(status)
+  const dispatch = useDispatch();
+  
   return (
-    <button type="button">Check status</button>
+    <div>
+      <button
+        type="button"
+        onClick={() => dispatch(checkStatus())}>
+        Check status
+     </button>
+      <h3>{status}</h3>
+    </div>
   );
-}
+};
 
 export default Catagories;
