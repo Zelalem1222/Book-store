aa79f32340749d14d761e396f47c6832d63418e6
Zelalem1222 <zelalemfissha394@gmail.com>
2022-07-20 13:51:38 -0400

    Modify books.js

--- src/components/books.js
+++ src/components/books.js
@@ -1,12 +1,25 @@
+/* eslint-disable */
 import React from 'react';
+import { useSelector } from 'react-redux';
 import AddBook from './addBook';
 import Book from './book';
 
+
 function Books() {
+  const books = useSelector((state) => state.books)
+  console.log(books);
   return (
     <div>
       <h1>Books</h1>
-      <Book title="Harry Potter" author="J.K. Rowling" />
+      {books.map((book) => (
+        <Book
+        key={book.id}
+        id={book.id}
+        title={book.title}
+        author={book.author}
+      />
+      ))}
+    
       <AddBook />
     </div>
   );
