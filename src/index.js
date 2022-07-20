137880893d414a5172a93d2503be4be13295f942
Zelalem1222 <zelalemfissha394@gmail.com>
2022-07-20 13:52:12 -0400

    Modify index.js

--- src/index.js
+++ src/index.js
@@ -7,12 +7,15 @@ import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <Provider store={store}>
       <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 );
