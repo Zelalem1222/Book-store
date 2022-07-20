4282915337d4c1b993c62962f8de8f000b8c7453
Zelalem1222 <zelalemfissha394@gmail.com>
2022-07-20 13:50:45 -0400

    Add the action to the add book

--- src/components/addBook.js
+++ src/components/addBook.js
@@ -1,11 +1,49 @@
-import React from 'react';
+/* eslint-disable */
+import React , { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid'; 
+import { addBook } from '../redux/books/books';
 
-const AddBook = () => (
-  <form>
-    <input type="text" placeholder="title" />
-    <input type="text" placeholder="author" />
-    <button type="submit">Add Book</button>
+
+const AddBook = () => {
+
+  const [inputText, setInputText] = useState({
+    title: '',
+    author: '',
+  });
+
+  const handleChange = (e) => {
+    setInputText({
+      ...inputText,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const newBook = {
+    id: uuidv4(),
+    title: inputText.title,
+    author: inputText.author,
+  };
+  const dispatch = useDispatch();
+  const handelAddBook = ()=> {
+   dispatch(addBook(newBook))
+   console.log(newBook)
+  }
+
+ const handleSubmit = (e) => {
+  e.preventDefault();
+  setInputText({title:'' , author: ''})
+ }
+ 
+
+ 
+return (
+  <form onSubmit={handleSubmit}>
+    <input value={inputText.title} name='title' type="text" onChange={handleChange} placeholder="title" />
+    <input value={inputText.author} name='author' type="text" onChange={handleChange} placeholder="author" />
+    <button type="submit" onClick={handelAddBook}>Add Book</button>
   </form>
-);
+  )
+};
 
 export default AddBook;
